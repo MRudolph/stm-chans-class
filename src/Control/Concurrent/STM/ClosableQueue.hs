c590a4e7e4e67df189fe05aa9917aa8429345018
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-10-02 15:22:56 +0200

    class for closable queues

---
+++ src/Control/Concurrent/STM/ClosableQueue.hs
@@ -0,0 +1,18 @@
+module Control.Concurrent.STM.Queue where
+
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TBMQueue
+import Control.Concurrent.STM.TMQueue
+
+
+class ClosableQueue q where
+  isClosedQueue :: q -> STM Bool
+  closeQueue :: q -> STM ()
+  
+instance ClosableQueue (TMQueue a) where
+  isClosedQueue = isClosedTMQueue
+  closeQueue = closeTMQueue
+
+instance ClosableQueue (TBMQueue a) where
+  isClosedQueue = isClosedTBMQueue
+  closeQueue = closeTBMQueue
